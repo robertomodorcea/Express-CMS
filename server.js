ea9c45f757322e680309f96872786d5c0edaea95
robertomodorcea <103879004+robertomodorcea@users.noreply.github.com>
2022-10-16 19:46:41 +0300

    Added Articles Router & EJS Render

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
+
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Welcome to Roberto's Blog")
+    res.render('index');
 })
 
+app.use('/articles', articleRouter);
 app.listen(2200);
